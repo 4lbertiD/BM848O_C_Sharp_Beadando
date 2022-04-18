814e372a44d0693b03588e6b1d07832e2097ccaa
Alberti <Alberti@DESKTOP-VHB4V8U>
2022-04-18 18:19:53 +0200

    Add project files.

---
+++ BM848O_C_Sharp_Beadando/Collection.cs
@@ -0,0 +1,61 @@
+﻿namespace BM848O_C_Sharp_Beadando
+{
+    using System;
+    using System.Collections.Generic;
+    public class Collection<T> where T : class
+    {
+
+        HashSet<int> halmaz1 = new HashSet<int>(new int[] { 2, 3, 4, 5, 6, 8, 1, 1, 10, 12 });
+        HashSet<int> halmaz2 = new HashSet<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+
+        public HashSet<int> Halmaz1 { get => halmaz1; set => halmaz1 = value; }
+        public HashSet<int> Halmaz2 { get => halmaz2; set => halmaz2 = value; }
+
+        internal void Add(int v)
+        {
+            halmaz1.Add(v);
+        }
+        internal void Remove(int v)
+        {
+            halmaz1.Remove(v);
+        }
+        internal void Unio()
+        {
+            halmaz1.UnionWith(halmaz2);
+        }
+        internal void Metszet()
+        {
+            halmaz2.IntersectWith(halmaz1);
+        }
+        internal void Count()
+        {
+            int db1 = halmaz1.Count();
+            int db2 = halmaz2.Count();
+            Console.WriteLine("Az elemek darabszama: \nA:\n{0} \nB:\n{1}", db1, db2);
+        }
+        internal void Kozos()
+        {
+            if (halmaz1.Overlaps(halmaz2))
+            {
+                Console.WriteLine("Igen");
+            }
+            else
+            {
+                Console.WriteLine("Nem");
+            }
+        }
+        internal void Megegyezo()
+        {
+            if (halmaz1.SetEquals(halmaz2))
+            {
+                Console.WriteLine("Igen");
+            }
+            else
+            {
+                Console.WriteLine("Nem");
+            }
+        }
+
+
+    }
+}
---
+++ BM848O_C_Sharp_Beadando/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace BM848O_C_Sharp_Beadando
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection<Program> collection = new Collection<Program>();
+
+            Console.WriteLine("A ket kezdo halmazunk:");
+            Console.WriteLine("A:");
+            Kiir(collection.Halmaz1);
+            Console.WriteLine("B:");
+            Kiir(collection.Halmaz2);
+
+            Console.WriteLine("Itt mindkét halmazhoz hozzaadunk elemeket:");
+            collection.Halmaz1.Add(12); // ez nem adódik hozzá, mivel már szerepel a halmazunkban
+            collection.Halmaz1.Add(15);
+            collection.Halmaz2.Add(16);
+            collection.Halmaz2.Add(10);
+            collection.Halmaz2.Add(12);
+            collection.Halmaz2.Add(19);
+            Console.WriteLine("A:");
+            Kiir(collection.Halmaz1);
+            Console.WriteLine("B:");
+            Kiir(collection.Halmaz2);
+
+            Console.WriteLine("Itt mindkét halmazból elveszünk elemeket:");
+            collection.Halmaz1.Remove(12);
+            collection.Halmaz2.Remove(12);
+            Console.WriteLine("A:");
+            Kiir(collection.Halmaz1);
+            Console.WriteLine("B:");
+            Kiir(collection.Halmaz2);
+
+            Console.WriteLine("A ket halmaz unioja:");
+            collection.Unio();
+            Kiir(collection.Halmaz1);
+
+            Console.WriteLine("A ket halmaz metszete:");
+            collection.Metszet();
+            Kiir(collection.Halmaz2);
+
+            collection.Count();
+
+            Console.WriteLine("\nVan kozos elem a ket halmazban?");
+            collection.Kozos();
+
+            Console.WriteLine("\nMinden elem megegyezik a ket halmazban?");
+            collection.Megegyezo();
+        }
+        public static void Kiir<T>(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.Write("{0}, ", item);
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
